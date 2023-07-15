e460ce4692ad3b4103624bffe17fde34008a07f9
Rafael Cronemberger <rafael@berger.com.br>
2023-07-15 16:52:57 -0300

    Packet updates with build yamls

---
+++ src/Berger.Extensions.Cache/Helpers/CacheHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Berger.Extensions.Cache
+{
+    public static class CacheHelper
+    {
+        public static T Get<T>(this IMemoryCache cache, object key, bool expiration)
+        {
+           return cache.Get<T>(key);
+        }
+        public static T Set<T>(this IMemoryCache cache, object key, T value, bool expiration)
+        {
+            return cache.Set(key, value);
+        }
+    }
+}
